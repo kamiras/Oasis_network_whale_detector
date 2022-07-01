0907883f8afbe9ef701ed954a205595d7b638a96
kamiras <80554217+kamiras@users.noreply.github.com>
2022-07-01 23:16:14 +0200

    Column price added

--- example.py
+++ example.py
@@ -1,36 +1,43 @@
-#The reason of this file is to deploy the database correctly step by step
 # You have to remember that every 24 hours the whale.py script deletes all the data from the databe to keep it clean and more efficient
 
 import mysql.connector
+from datetime import datetime
+import tweepy
+import keys
+
+client = tweepy.Client(consumer_key=keys.API_KEY,
+                        consumer_secret=keys.API_SECRET,
+                        access_token=keys.ACCES_TOKEN,
+                        access_token_secret=keys.ACCES_TOKEN_SECRET)
 
 mydb = mysql.connector.connect(
-  host="", # Insert your host
-  user="", # Insert your user
+  host="localhost", # Insert your host
+  user="root", # Insert your user
   password="", # Insert your password 
-  database="" # Insert your database, if you follow the example bellow the database would be 'oasis_database'
+  database="oasis_database" # Insert your database, if you follow the example bellow the database would be 'oasis_database'
 
 )
 
-mycursor = mydb.cursor()
+print(datetime.now().strftime("%H:%M"))
 
 ''' Creation of the the database, table and columns
 
 mycursor.execute("CREATE DATABASE oasis_database")
 
-mycursor.execute("CREATE TABLE datos (oasis_from VARCHAR(255), amount DECIMAL(32,2), time VARCHAR(16))")
+mycursor.execute("CREATE TABLE datos (oasis_from VARCHAR(255), amount DECIMAL(32,2), time VARCHAR(16), price DECIMAL(32,2))")
 
 '''
 
 ''' We commit 2 examples of data strings to check if the database works
 
-sql = "INSERT INTO datos (oasis_from, amount, time) VALUES (%s, %s, %s)"
-val = ("oasis1qzwfdgpt3p6dd2mk6207qhf9qrxuvfn9eunurdgu", "4867.90", "10:58:07")
+sql = "INSERT INTO datos (oasis_from, amount, time) VALUES (%s, %s, %s, %s)"
+val = ("oasis1qzwfdgpt3p6dd2mk6207qhf9qrxuvfn9eunurdgu", "4867.90", "10:58:07", "2000")
 mycursor.execute(sql, val)
 
 mydb.commit()
 
 sql = "INSERT INTO datos (oasis_from, amount, time) VALUES (%s, %s, %s)"
-val = ("oasis1qzwfdgpt3p6dd2mk6207qhf9qrxuvfn9eunurdgu", "3867.90", "10:58:10")
+val = ("oasis1qzwfdgpt3p6dd2mk6207qhf9qrxuvfn9eunurdgu", "3867.90", "10:58:10", "4000")
 mycursor.execute(sql, val)
 
 mydb.commit()
@@ -38,7 +45,7 @@ mydb.commit()
 '''
 
 
-''' Here we can see the first highest transaction of the database
+''' Here we can see the first the highest transaction of the database
 
 mycursor.execute("SELECT * FROM datos ORDER BY amount DESC")
 
