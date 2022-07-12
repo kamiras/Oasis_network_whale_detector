2e592e562b6c8d25e7c22fc7961ec0d955fa2ced
kamiras <80554217+kamiras@users.noreply.github.com>
2022-07-12 21:55:41 +0200

    EXTRA_BEARER_CODE

--- keys.py
+++ keys.py
@@ -3,3 +3,4 @@ API_SECRET = '' # Your API_SECRET
 BEARER_TOKEN = '' # Your BEARER_TOKEN
 ACCES_TOKEN = '' # Your ACCES_TOKEN
 ACCES_TOKEN_SECRET = '' # Your ACCES_TOKEN_SECRET
+EXTRA_BEARER_CODE = '' # Only available in Elevate level of Twitter
